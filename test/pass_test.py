09e85c06a912e99eab6fe3a2132624f51ea493f1
fanyangyang <1007672393@qq.com>
2016-11-14 12:25:32 +0800

    fisrt add

---
+++ test/pass_test.py
@@ -0,0 +1,10 @@
+x=input('please enter:')
+
+if x == 'aaa':
+    pass
+elif x=='bbb':
+    pass
+elif x=='ccc':
+    print('ok')
+else :
+    print('no')
---
+++ test/web_test.py
@@ -0,0 +1,9 @@
+def dan_abs(x):
+    if x>= 0:
+        return x
+    else:
+        return -x
+
+x=int(input('please enter:'))
+
+print(dan_abs(x))
